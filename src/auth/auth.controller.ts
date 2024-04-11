ad8197ea1d18256721ba1518019c6e9eb0edb674
jbjiang <jbjiang@kland.com.cn>
2024-04-11 17:49:21 +0800

    处理用户登录密码明文显示

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from "@nestjs/passport";
 import type { Request } from 'express';
 import { AuthService } from "./auth.service";
 import { Public } from "../global/decorator/public.decorator";
-import { ApiQuery } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiQuery } from "@nestjs/swagger";
 
 @Controller('auth')
 export class AuthController {
@@ -14,12 +14,18 @@ export class AuthController {
   @Public()
   @Post('login')
 
-  @ApiQuery({ name: 'password', type: String, required: true, description: '用户密码' })
+  @ApiQuery({ name: 'password', type: String, required: true,
+    schema: {
+      type: 'string',
+      format: 'password', // 使用format属性指定密码格式
+    },
+    description: '用户密码' })
   @ApiQuery({ name: 'username', type: String, required: true, description: '用户账号' })
   login(@Req() req: Request) {
     return this.authService.login(req.user);
   }
 
+  @ApiBearerAuth()
   @Delete('logout')
   logout(@Req() req: Request) {
     return this.authService.logout(req.user);
